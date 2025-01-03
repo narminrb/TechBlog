95ef5948205f1f094e0aec72a501897c43fb3acb
Narmin <nbalakishi@gmail.com>
2025-01-04 03:20:45 +0400

    changed buttons

--- src/components/CustomStoryPagination/index.jsx
+++ src/components/CustomStoryPagination/index.jsx
@@ -3,10 +3,25 @@ import { Link } from 'react-router-dom';
 
 const CustomStoryPagination = ({ image, name, desc, url }) => {
   return (
-    <div className="flex w-full md:max-w-xl mx-4 rounded shadow flex-nowrap">
+    <div
+      className="flex w-full  shadow flex-nowrap justify-center text-center"
+      style={{
+        border:"1px solid #ced0d4",
+        borderRadius: '6px',
+        padding: '10px 20px',
+        height: '46px',
+        alignItems:'center',
+        fontWeight: '700',
+        fontSize: '18px',
+        color: '#878787',
+        background: '#FFFFFF',
+        outline: 'none',
+        cursor: 'pointer',
+      }}
+    >
       <Link
         to={url}
-        className="w-full flex justify-center font-medium rounded-l px-5 py-2 border bg-white text-gray-800 border-gray-200 hover:bg-gray-100"
+        className=""
       >
         {name}
       </Link>
